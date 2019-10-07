6b84906e2c45421607e113824368bdf2fa642d1f
OlgaBikova <olgabikova@inbox.lv>
2019-10-07 14:37:25 +0300

    Add files via upload

---
+++ 9_lesson/WEBApiExample1/WEBApiExample2/Controllers/EmployeeController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+using WEBApiExample2.Models;
+
+namespace WEBApiExample2.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        // GET: api/Employee
+        [HttpGet]
+        public IEnumerable<EmployeeModel> EmployeeGet()
+        {
+            return GetEmployeeListFromXml();
+            //return GetEmployeeList();
+        }
+              
+        // GET: api/Employee/5
+        [HttpGet]
+        public IEnumerable<EmployeeModel> Get(int id)
+        {
+            var employeeList = GetEmployeeListFromXml();
+
+            return employeeList.Where(x => x.Id == id);
+        }
+
+        // POST: api/Employee
+        [HttpPost]
+        public void Post([FromBody]EmployeeModel employee)
+        {
+            var fullPath = System.Web.Hosting.HostingEnvironment.MapPath(@"~/App_data/EmployeeList.xml");
+
+            XElement xElement = new XElement("EmployeeModel",
+                new XElement("Id", employee.Id),
+                new XElement("Name", employee.Name),
+                new XElement("Surname", employee.Surname),
+                new XElement("Age", employee.Age));
+
+            var existingXmlDocument = XDocument.Load(fullPath);
+            existingXmlDocument.Root.Add(xElement);
+            existingXmlDocument.Save(fullPath);
+        }
+
+        // PUT: api/Employee/5
+        [HttpPut]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Employee/5
+        [HttpDelete]
+        public void DeleteEmployeeRecord(int id)
+        {
+        }
+
+        private List<EmployeeModel> GetEmployeeList()
+        {
+            var employeeList = new List<EmployeeModel>();
+
+            var employee1 = new EmployeeModel() { Id = 1, Name = "Tom", Surname = "Johnos", Age = 23 };
+            var employee2 = new EmployeeModel() { Id = 2, Name = "Bill", Surname = "Gates", Age = 45 };
+            var employee3 = new EmployeeModel() { Id = 3, Name = "Kate", Surname = "Svenson", Age = 65 };
+            var employee4 = new EmployeeModel() { Id = 4, Name = "Lisa", Surname = "Olson", Age = 21 };
+
+            employeeList.Add(employee1);
+            employeeList.Add(employee2);
+            employeeList.Add(employee3);
+            employeeList.Add(employee4);
+
+            return employeeList;
+        }
+
+        private List<EmployeeModel> GetEmployeeListFromXml()
+        {
+            var fullPath = System.Web.Hosting.HostingEnvironment.MapPath(@"~/App_data/EmployeeList.xml");
+
+            XmlRootAttribute xRoot = new XmlRootAttribute();
+            xRoot.ElementName = "EmployeeList";
+
+            XmlSerializer serializer = new XmlSerializer(typeof(List<EmployeeModel>), xRoot);
+
+            System.IO.StreamReader reader = new System.IO.StreamReader(fullPath);
+
+            var employeeList = (List<EmployeeModel>)serializer.Deserialize(reader);
+
+            reader.Close();
+            return employeeList;
+        }
+    }
+}
---
+++ 9_lesson/WEBApiExample1/WEBApiExample2/Scripts/EmployeePage.js
@@ -0,0 +1,131 @@
+﻿var url = "api/Employee";
+var lastId;
+
+//$(document).ready(function () {
+//    $.getJSON(url).done(function (data) {
+//        $.each(data, function (key, item) {
+//            $('<li>', { text: formatEmployeeName(item) }).appendTo($("#employeeList"));
+//        });
+//    });
+//});
+
+$(document).ready(function () {
+    GetAllEmployees();
+
+    $('#editEmployee').click(function () {
+        EditEmployee();
+    });
+
+    $('#addEmployee').click(function () {
+        CreateNewEmployee();
+    });
+});
+
+function GetAllEmployees() {
+
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: "json",
+        success: function (data) {
+            var strResult = "<table><th>ID</th><th>Name</th><th>Surname</th><th>Age</th>";
+            $.each(data, function (index, item) {
+                strResult += "<tr>" +
+                    "<td>" + item.Id + "</td>" +
+                    "<td>" + item.Name + "</td>" +
+                    "<td>" + item.Surname + "</td>" +
+                    "<td>" + item.Age + "</td>" +
+                    "<td>" +
+                    "<a id='editItem' data-item='" + item.Id + "' onclick='EditItem(this);' >Update</a>" +
+                    "</td>" +
+                    "<td>" +
+                    "<a id='delItem' data-item='" + item.Id + "' onclick='DeleteItem(this);' >Delete</a>" +
+                    "</td>" +
+                    "</tr>";
+                lastId = item.Id + 1;
+            });
+            strResult += "</table>";
+            $("#tableBlock").html(strResult);
+        },
+        error: function (x, y, z) {
+            alert(x + '\n' + y + '\n' + z);
+        }
+    });
+}
+
+function EditItem(el)
+{
+    var id = $(el).attr('data-item');
+    GetEmployee(id);
+}
+
+function GetEmployee(id)
+{
+    $.ajax({
+        url: url +"/"+id,
+        type: 'GET',
+        dataType: "json",
+        success: function (data) {
+            ShowEmployee(data);
+        },
+        error: function (x, y, z) {
+            alert(x + '\n' + y + '\n' + z);
+        }
+    });
+}
+
+function ShowEmployee(data)
+{
+    $("#EmployeeEditBlock").css('display', 'block');
+    $("#EmployeeCreateBlock").css('display', 'none');
+
+    $("#editId").val(data[0].Id);
+    $("#editName").val(data[0].Name);
+    $("#editSurname").val(data[0].Surname);
+    $("#editAge").val(data[0].Age);
+}
+
+function EditEmployee() { }
+
+function CreateNewEmployee() {
+    var employee =
+    {
+        Id: lastId,
+        Name: $('#addName').val(),
+        Surname: $('#addSurname').val(),
+        Age: $('#addAge').val()
+    };
+
+    $.ajax({
+        url: url,
+        type: 'POST',
+        data: JSON.stringify(employee),
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            GetAllEmployees();
+        },
+        error: function (x, y, z) {
+            alert(x + '\n' + y + '\n' + z);
+        }
+    });
+}
+
+
+
+function formatEmployeeName(item) {
+    return item.Name + " " + item.Surname;
+}
+
+function find() {
+    var idValue = $("#employeeId").val();
+    var fullUrl = url + "/" + idValue;
+
+    $.getJSON(fullUrl)
+        .done(function (data) {
+            $("#employeeList").text(formatEmployeeName(data[0]))
+        })
+        .fail(function (jqXHR, textStatus, err) {
+            $("#employeeList").text('Error: ' + err);
+        });
+
+}
